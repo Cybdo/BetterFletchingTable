9f78c73e09a7d0b5cde75446570b73a732df0008
Ryan <ryan@cybdo.me>
2025-08-17 22:09:05 +1000

    Set up the repo
    DEVLOG 1!!!
    
    IDE set up, github set up, ideas made, hello world works.
    
    not much else tbh
    
    i had to restart i accidentally deleted everything

---
+++ src/main/java/me/cybdo/better_fletching_table/Better_fletching_table.java
@@ -0,0 +1,10 @@
+package me.cybdo.better_fletching_table;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Better_fletching_table implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/cybdo/better_fletching_table/client/Better_fletching_tableClient.java
@@ -0,0 +1,11 @@
+package me.cybdo.better_fletching_table.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Better_fletching_tableClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Hello, World!");
+    }
+}
